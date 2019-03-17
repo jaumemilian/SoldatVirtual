227ceff2aec28bbfa3549f941de8b3363a34124e
Jaume Milian <jaume.milian@asg.com>
2019-03-17 08:20:22 +0100

    Nou Soldat Virtual i capacitat inicial de moures per la taula

--- src/Soldat Virtual/Assets/Soldat Virtual/Scripts/SoldatVirtualController.cs
+++ src/Soldat Virtual/Assets/Soldat Virtual/Scripts/SoldatVirtualController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using GoogleARCore;
+using UnityEngine.AI;
 
 #if UNITY_EDITOR
-    using Input = GoogleARCore.InstantPreviewInput;
+using Input = GoogleARCore.InstantPreviewInput;
 #endif
 
 public class SoldatVirtualController : MonoBehaviour
 {
-    public GameObject GridPrefab;
-
     public GameObject Soldier;
+
+    public Animator animator;
 
     public GameObject ArCamera;
 
@@ -21,13 +22,70 @@ public class SoldatVirtualController : MonoBehaviour
     /// </summary>
     private bool m_IsQuitting = false;
 
+    public float turnSmoothing = 15f;           // The amount of smoothing applied to the player's turning using spherical interpolation.
+
+    private Vector3 destination;
+
+    private readonly int hashSpeedPara = Animator.StringToHash("Speed");
+
+    private bool soldierIsStopped = true;
+
     /// <summary>
     /// The Unity Update() method.
     /// </summary>
     public void Update()
     {
         _UpdateApplicationLifecycle();
+        //TODO: JMMPEND: Show here to manage the first touch, and display the whole scenario
 
+        _UpdateSoldier();        
+        _OnTapHandler();
+    }
+
+    private void _UpdateSoldier()
+    {
+        float speed = 0;
+
+        if (Soldier.activeInHierarchy)
+        {
+            if (Vector3.Distance(Soldier.transform.position, destination) <= 0.01)
+            {
+                if (!soldierIsStopped)
+                {
+                    MessageManager.ShowAndroidToastMessage("Stopping from main: ");
+                    
+                    // Set the player's position to the destination.
+                    Soldier.transform.position = destination;
+
+                    // Set the speed (which is what the animator will use) to zero.
+                    speed = 0f;
+
+                    soldierIsStopped = true;
+                }
+            }
+            else
+            {
+                speed = 1f;
+
+                MessageManager.ShowAndroidToastMessage("Moving from main." + 
+                "Speed: " + speed +" > " + 0 + ". " +
+                "Position: " + Soldier.transform.position + ". " + 
+                "Destination: " + destination);
+
+                 // Rotate the soldier to face the camera
+                Soldier.transform.LookAt(destination, Soldier.transform.up);
+
+                Soldier.transform.position = Vector3.MoveTowards(Soldier.transform.position, destination, 0.01f);
+            }
+
+            // Set the animator's Speed parameter based on the (possibly modified) speed that the nav mesh agent wants to move at.
+            animator.SetFloat(hashSpeedPara, speed, 0.1f, Time.deltaTime);
+        }
+    }
+
+    private void _OnTapHandler()
+    {
+        // Detect if a touch occurs
         Touch touch;
 
         if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
@@ -39,29 +97,42 @@ public class SoldatVirtualController : MonoBehaviour
         TrackableHit hit;
         if (Frame.Raycast(touch.position.x, touch.position.y, TrackableHitFlags.PlaneWithinPolygon,out hit))
         {
-            // Let's place the soldier 
+            if (Soldier.activeInHierarchy)
+            {
+                // Set the destination
+                destination = hit.Pose.position;
+                soldierIsStopped = false;
+                MessageManager.ShowAndroidToastMessage("Mueve al soldado: " + hit.Pose.position);
+            }
+            else
+            {
+                // Enable the soldier
+                Soldier.SetActive(true);
 
-            // Enable the soldier
-            Soldier.SetActive(true);
+                // Create a new Anchor
+                Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+                
+                // Set the position of the soldier
+                Soldier.transform.position = hit.Pose.position;
+                
+                MessageManager.ShowAndroidToastMessage("Muestra el soldado: " + hit.Pose.position);
 
-            // Create a new Anchor
-            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
-            
-            // Set the position of the soldier
-            Soldier.transform.position = hit.Pose.position;
-            Soldier.transform.rotation = hit.Pose.rotation;
+                Soldier.transform.rotation = hit.Pose.rotation;
 
-            // Show the soldier to face the camera
-            Vector3 cameraPosition = ArCamera.transform.position;
+                // Show the soldier to face the camera
+                Vector3 cameraPosition = ArCamera.transform.position;
 
-            // The soldier should only rotate around the Y axis
-            cameraPosition.y = hit.Pose.position.y;
+                // The soldier should only rotate around the Y axis
+                cameraPosition.y = hit.Pose.position.y;
 
-            // Rotate the soldier to face the camera
-            Soldier.transform.LookAt(cameraPosition, Soldier.transform.up);
+                // Rotate the soldier to face the camera
+                Soldier.transform.LookAt(cameraPosition, Soldier.transform.up);
 
-            // Need to attach our Soldier to the anchor
-            Soldier.transform.parent = anchor.transform;
+                // Need to attach our Soldier to the anchor
+                Soldier.transform.parent = anchor.transform;
+
+                destination = Soldier.transform.position;
+            }
         }
     }
 
@@ -95,13 +166,13 @@ public class SoldatVirtualController : MonoBehaviour
         // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
         if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
         {
-            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            MessageManager.ShowAndroidToastMessage("Camera permission is needed to run this application.");
             m_IsQuitting = true;
             Invoke("_DoQuit", 0.5f);
         }
         else if (Session.Status.IsError())
         {
-            _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+            MessageManager.ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
             m_IsQuitting = true;
             Invoke("_DoQuit", 0.5f);
         }
@@ -113,26 +184,5 @@ public class SoldatVirtualController : MonoBehaviour
     private void _DoQuit()
     {
         Application.Quit();
-    }
-
-    /// <summary>
-    /// Show an Android toast message.
-    /// </summary>
-    /// <param name="message">Message string to show in the toast.</param>
-    private void _ShowAndroidToastMessage(string message)
-    {
-        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-
-        if (unityActivity != null)
-        {
-            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
-            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
-            {
-                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
-                    message, 0);
-                toastObject.Call("show");
-            }));
-        }
     }
 }
---
+++ src/Soldat Virtual/Assets/Soldat Virtual/Scripts/SoldierMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoldierMovement : MonoBehaviour
+{
+    private void Start()
+    {
+        MessageManager.ShowAndroidToastMessage("Soldier Movement Start");
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    // This function is called by the EventTrigger on the scene's ground when it is clicked on.
+    public void OnGroundClick(BaseEventData data)
+    {
+        MessageManager.ShowAndroidToastMessage("Soldier Movement - OnGroundClick");
+    }
+}
