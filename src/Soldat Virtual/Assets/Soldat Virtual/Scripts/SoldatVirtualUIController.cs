bf2b917c55d5a7441e7632841c4d065d89ef5331
Jaume Milian <jaume.milian@asg.com>
2019-04-04 19:17:38 +0200

    Show a button in the screen

---
+++ src/Soldat Virtual/Assets/Soldat Virtual/Scripts/SoldatVirtualUIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoldatVirtualUIController : MonoBehaviour
+{
+    /// <summary>
+    /// Scenario1 Button for toggling model
+    /// </summary>
+    public Button RunModeButton;
+
+    /// <summary>
+    /// Scenario1 Button for toggling model
+    /// </summary>
+    public Button ShotModeButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
